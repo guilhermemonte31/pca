199e2e0ba88c36d3d02472fbb665a795c65da32d
guilhermemonte31 <120525923+guilhermemonte31@users.noreply.github.com>
2023-03-20 17:45:29 -0400

    Update main.c

--- ponteiros-2/main.c
+++ ponteiros-2/main.c
@@ -1,10 +1,11 @@
 /*20230314_001
 Faça um programa que calcula a média e o desvio padrão das notas de uma turma;
-- O programa deve solicitar no início a quantidade de alunos na turma e alocar dinamicamente um vetor de float onde as
-  notas dos alunos digitadas devem ser armazenadas;
-- Você deve criar uma função “mediaDesvio” para calcular a média e o desvio padrão e retornar estes valores através de parâmetros
-  passados por referência (ponteiros). A função recebe como parâmetro o ponteiro para o vetor,
-  a quantidade de elementos no vetor, e dois ponteiros: media e desvio onde deve guardar os resultados;*/
+- O programa deve solicitar no início a quantidade de alunos na turma e alocar dinamicamente um vetor 
+de float onde as notas dos alunos digitadas devem ser armazenadas;
+- Você deve criar uma função “mediaDesvio” para calcular a média e o desvio padrão e retornar 
+estes valores através de parâmetros passados por referência (ponteiros). A função recebe como 
+parâmetro o ponteiro para o vetor, a quantidade de elementos no vetor, e dois ponteiros: 
+media e desvio onde deve guardar os resultados;*/
 
 
 #include <stdio.h>
@@ -12,20 +13,23 @@ Faça um programa que calcula a média e o desvio padrão das notas de uma turma
 #include <math.h>
 
 void mediaDesvio(float *notas, int alunos,float *md,float *dp ){
-        float soma, media;
-        int i;
+    float soma, teste;
+    int i;
 
-        for(i=0; i<alunos; i++){
-            soma += notas[i];
-        }
-        *md = soma/alunos;
-        printf("MEDIA= %.1f\n", *md);
-        
-        
-        soma=0;
-        for(i=0; i<alunos; i++){
-            soma = pow
-        }
+    for(i=0; i<alunos; i++){
+        soma += notas[i];
+    }
+    *md = soma/alunos;
+    printf("MEDIA= %.1f\n", *md);
+    
+    teste = *md;
+    soma=0;
+    for(i=0; i<alunos; i++){
+        soma+= pow((notas[i]-teste), 2);
+    }
+    *dp = sqrt(soma/alunos);
+    printf("DESVIO PADRAO= %.1f", *dp);
+    
         
 
 }
@@ -34,7 +38,7 @@ int main()
 {
     int  i, alunos;
     float *notas;
-    float soma=0, md, dp;
+    float md, dp;
 
     printf("Digite a quantidade de alunos: ");
     scanf("%i", &alunos);
