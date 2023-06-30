084f74ef53e1cc37333210e73181b6b01277cae7
guilhermemonte31 <120525923+guilhermemonte31@users.noreply.github.com>
2023-06-30 16:56:29 -0400

    Add files via upload

---
+++ Posfixo/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "posfixo.h"
+
+int main(){
+    char infixa[50];
+    printf("Escreva uma expressao matematica: ");
+    scanf(" %[^\n]", &infixa);
+    imprimeExpressao(infixa);
+    char *posfixa;
+    int t = strlen(infixa);
+    posfixa = malloc(t* sizeof(char));
+    posfixa = montaPosfixa(infixa);
+    printf("\nPOSFIXA FINAL= %s", posfixa);
+
+    float resultado;
+    resultado = CalculaPosfixa(posfixa);
+    printf("\nRESULTADO DA POSFIXA: %f", resultado);
+    
+}
+
